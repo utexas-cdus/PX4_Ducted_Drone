ffe7f8c23aa3b5ececa1d7bff02d172a00a65171
JeffreyYoung34 <jeffrey.a.young@utexas.edu>
2025-06-03 13:22:19 -0500

    Adding in Ducted Drone VTOL Config WIP

---
+++ src/modules/control_allocator/ActuatorEffectiveness/ActuatorEffectivenessDuctedDroneVTOL.cpp
@@ -0,0 +1,107 @@
+/****************************************************************************
+ *
+ *   Copyright (c) 2020 PX4 Development Team. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ * 3. Neither the name PX4 nor the names of its contributors may be
+ *    used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS
+ * OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+ * AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ ****************************************************************************/
+
+/**
+ * @file ActuatorEffectivenessTiltingMultirotor.hpp
+ *
+ * Actuator effectiveness computed from rotors position and orientation
+ *
+ * @author Jeffrey Young <jeffrey.a.young34@gmail.com>
+ */
+
+#include "ActuatorEffectivenessDuctedDroneVTOL.hpp"
+
+using namespace matrix;
+
+ActuatorEffectivenessDuctedDroneVTOL::ActuatorEffectivenessDuctedDroneVTOL(ModuleParams *parent)
+	: ModuleParams(parent)
+{
+	_servo_count_handle = param_find("CA_SV_TL_COUNT");
+
+	updateParams();
+}
+
+void ActuatorEffectivenessDuctedDroneVTOL::updateParams()
+{
+	ModuleParams::updateParams();
+
+	// if (param_get(_tilting_type_handle, &_tilting_type) != 0) {
+	// 	PX4_ERR("param_get failed");
+	// 	return;
+	// }
+
+	if (param_get(_servo_count_handle, &_servo_count) != 0) {
+		PX4_ERR("param_get failed");
+		return;
+	}
+
+	// Instatiate duct_rotors of class ActuatorEffectivenessRotors and duct_servos of class ActuatorEffectivenessServos
+	duct_rotors = new ActuatorEffectivenessRotorsDucted(this);
+	duct_servos = new ActuatorEffectivenessControlSurfacesDucted(this);
+}
+
+bool
+ActuatorEffectivenessDuctedDroneVTOL::getEffectivenessMatrix(Configuration &configuration,
+		EffectivenessUpdateReason external_update)
+{
+	if (external_update == EffectivenessUpdateReason::NO_EXTERNAL_UPDATE) {
+		return false;
+	}
+
+	// Motors
+	// Update Effectiveness Matrix with Cyclic Pitch based on the Servo Input Configuration Parameter 
+	// Note: configuration updates are only possible when disarmed.
+
+	bool rotors_added_successfully = false;
+	bool servos_added_successfully = false; 
+
+	//Rotors & Servos 
+	configuration.selected_matrix = 0;
+	rotors_added_successfully = duct_rotors->addActuators(configuration);
+
+	// Servos 
+	configuration.selected_matrix = 1;
+	servos_added_successfully = duct_servos->addActuators(configuration);
+	return (rotors_added_successfully && servos_added_successfully);
+}
+
+// Commenting out because not needed 
+// void ActuatorEffectivenessDuctedDroneVTOL::updateSetpoint(const matrix::Vector<float, NUM_AXES> &control_sp, int matrix_index,
+// 		ActuatorVector &actuator_sp)
+// {
+// 	for (int i = 0; i < _servo_count; i++) {
+// 		actuator_sp(i) = actuator_sp(i) < math::radians(_servo_param[i].angle_min) ? math::radians(_servo_param[i].angle_min) : actuator_sp(i);
+// 		actuator_sp(i) = actuator_sp(i) > math::radians(_servo_param[i].angle_max) ? math::radians(_servo_param[i].angle_max) : actuator_sp(i);
+// 	}
+// }
+
---
+++ src/modules/control_allocator/VehicleActuatorEffectiveness/ActuatorEffectivenessDuctedDroneVTOL.hpp
@@ -0,0 +1,105 @@
+/****************************************************************************
+ *
+ *   Copyright (c) 2020-2022 PX4 Development Team. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ * 3. Neither the name PX4 nor the names of its contributors may be
+ *    used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS
+ * OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+ * AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ ****************************************************************************/
+
+#pragma once
+
+#include "ActuatorEffectiveness.hpp"
+#include "ActuatorEffectivenessRotors.hpp"
+#include "ActuatorEffectivenessControlSurfacesDucted.hpp"
+#include "ActuatorEffectivenessRotorsDucted.hpp"
+
+// #include "ActuatorEffectivenessTilts.hpp" // Commenting out 
+#include <px4_platform_common/module_params.h>
+
+#include <uORB/topics/actuator_controls.h>
+#include <uORB/Subscription.hpp>
+
+class ActuatorEffectivenessDuctedDroneVTOL : public ModuleParams, public ActuatorEffectiveness
+{
+public:
+	ActuatorEffectivenessDuctedDroneVTOL(ModuleParams *parent);
+	virtual ~ActuatorEffectivenessDuctedDroneVTOL() = default;
+
+	/**
+     * Populate the effectiveness matrix for cyclic pitch servos
+     */
+	bool getEffectivenessMatrix(Configuration &configuration, EffectivenessUpdateReason external_update) override;
+
+    int numMatrices() const override { return 1; }
+
+	/** The only valid Control Allocation method is Pseudo Inverse */
+	void getDesiredAllocationMethod(AllocationMethod allocation_method_out[MAX_NUM_MATRICES]) const override
+	{	
+		allocation_method_out[0] = AllocationMethod::PSEUDO_INVERSE;
+	}
+
+	void getNormalizeRPY(bool normalize[MAX_NUM_MATRICES]) const override
+	{
+		normalize[0] = true;
+
+	}
+
+	// void updateSetpoint(const matrix::Vector<float, NUM_AXES> &control_sp, int matrix_index,
+	// 		    ActuatorVector &actuator_sp) override;
+
+	const char *name() const override { return "Ducted Drone VTOL "; }
+
+protected:
+	void updateParams() override;
+	ActuatorEffectivenessRotorsDucted *duct_rotors{nullptr};
+	ActuatorEffectivenessControlSurfacesDucted *duct_servos{nullptr};
+
+	static constexpr int NUM_SERVOS_MAX = 2;
+
+	struct ServoParamHandles{
+		param_t torque_x;
+    	param_t torque_y;
+    	param_t torque_z;
+
+    	param_t thrust_x;
+    	param_t thrust_y;
+    	param_t thrust_z;
+	};
+	param_t _servo_count_handle;
+	int32_t _servo_count{0};
+
+	struct ServoParam{
+		matrix::Vector3f servo_torque_gain;
+		matrix::Vector3f servo_thrust_gain;
+	};
+	
+	ServoParamHandles _servo_param_handles[NUM_SERVOS_MAX];
+	ServoParam _servo_param[NUM_SERVOS_MAX];
+
+	uORB::Subscription _actuator_controls_0_sub{ORB_ID(actuator_controls_0)};
+};
